db702c577da84e98992419896a4defe3a990f047
JorgeFVS <jorgevieira99@hotmail.com>
2024-08-16 18:23:39 -0300

    Variaveis Ponto Flutuante

---
+++ Curso/src/com/jorge/cursojava/aula2/VariaveisPontoFlutuante.java
@@ -0,0 +1,16 @@
+package com.jorge.cursojava.aula2;
+
+public class VariaveisPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double valorPassagem = 4.40;
+		
+		float valorTomate = 3.95f;
+		
+		System.out.println("Valor da passagem = " + valorPassagem);
+		System.out.println("Valor do Tomate = " + valorTomate);
+
+	}
+
+}
